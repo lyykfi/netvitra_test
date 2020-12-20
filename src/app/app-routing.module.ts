fb7e630d1c7cba3f3a692ea8486c407020f0b363
Andrei  Makar-Uvarov <lyykfi@gmail.com>
2020-12-20 17:23:49 +0100

    WIP

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { AuthGuard } from "./auth.guard";
+import { CasesComponent } from "./cases/cases.component";
+import { LoginComponent } from "./login/login.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: "", pathMatch: "full", redirectTo: "login" },
+    { path: "login", component: LoginComponent },
+    { path: "cases", component: CasesComponent, canActivate: [AuthGuard] }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
